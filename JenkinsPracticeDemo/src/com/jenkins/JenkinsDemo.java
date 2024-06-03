38bace584679101d81c4713f80ae19a9e71cf585
Nidhi-Pardhi <nidhipardhi2610@gmail.com>
2024-06-03 17:02:35 +0530

    Jenkins first commit

---
+++ JenkinsPracticeDemo/src/com/jenkins/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import java.util.Date;
+
+public class JenkinsDemo {
+
+    public static void main(String[] args) {
+        System.out.println("This is jenkins project");
+        System.out.println("output from  java program");
+        System.out.println(new Date());
+
+    }
+}
